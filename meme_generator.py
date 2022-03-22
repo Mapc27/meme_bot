334da664f2ace00cd0cfefd7f593deea1781ab77
Mapc <mapctrigotixd@gmail.com>
2022-03-22 19:46:37 +0300

    + own write captions functions

--- meme_generator.py
+++ meme_generator.py
@@ -118,92 +118,43 @@ class MemeGenerator:
         if len(self.authors) != 3:
             raise AttributeError("Authors number less or more than 3")
 
-        self.draw_text(self.top_caption, 'top')
-        self.draw_text(self.bottom_caption, 'bottom')
-        self.image.save(f"images/{self.file_name}")
+        self.write_top_caption()
+        self.write_bottom_caption()
+        self.image.save(f"{IMAGES_FOLDER_NAME}/{self.file_name}")
 
         return self.file_name
 
-    def draw_text_with_outline(self, text, x, y):
-        self.draw.text((x - 2, y - 2), text, (0, 0, 0), font=self.font)
-        self.draw.text((x + 2, y - 2), text, (0, 0, 0), font=self.font)
-        self.draw.text((x + 2, y + 2), text, (0, 0, 0), font=self.font)
-        self.draw.text((x - 2, y + 2), text, (0, 0, 0), font=self.font)
-        self.draw.text((x, y), text, (255, 255, 255), font=self.font)
-
-    def draw_text(self, text, pos):
+    def write_top_caption(self):
+        text = self.top_caption
         text = text.upper()
-        width, height = self.draw.textsize(text, self.font)
+        text_width, text_height = self.draw.textsize(text, self.font)
 
-        line_count = 1
-        if width > self.image.width:
-            line_count = int(round((width / self.image.width) + 1))
+        print(self.image.width, self.image.height)
+        print(text_width, text_height)
 
-        print("line_count: {}".format(line_count))
+        coord_x = (self.image.width - text_width) // 2
+        coord_y = 7
 
-        lines = []
-        if line_count > 1:
+        self.draw.text((coord_x, coord_y), text, (255, 255, 255), font=self.font)
 
-            last_cut = 0
-            is_last = False
-            for i in range(0, line_count):
-                if last_cut == 0:
-                    cut = (len(text) / line_count) * i
-                else:
-                    cut = last_cut
+    def write_bottom_caption(self):
+        text = self.bottom_caption
+        text = text.upper()
+        text_width, text_height = self.draw.textsize(text, self.font)
 
-                if i < line_count - 1:
-                    next_cut = (len(text) / line_count) * (i + 1)
-                else:
-                    next_cut = len(text)
-                    is_last = True
+        print(self.image.width, self.image.height)
+        print(text_width, text_height)
 
-                print("cut: {} -> {}".format(cut, next_cut))
+        coord_x = (self.image.width - text_width) // 2
+        coord_y = self.image.height - 7 - text_height
 
-                # make sure we don't cut words in half
-                if next_cut == len(text) or text[next_cut] == " ":
-                    print("may cut")
-                else:
-                    print("may not cut")
-                    while text[next_cut] != " ":
-                        next_cut += 1
-                    print("new cut: {}".format(next_cut))
-
-                line = text[cut:next_cut].strip()
-
-                # is line still fitting ?
-                width, height = self.draw.textsize(line, self.font)
-                if not is_last and width > self.image.width:
-                    print("overshot")
-                    next_cut -= 1
-                    while text[next_cut] != " ":
-                        next_cut -= 1
-                    print("new cut: {}".format(next_cut))
-
-                last_cut = next_cut
-                lines.append(text[cut:next_cut].strip())
-
-        else:
-            lines.append(text)
-
-        print(lines)
-
-        last_y = -height
-        if pos == "bottom":
-            last_y = self.image.height - height * (line_count + 1) - 10
-
-        for i in range(0, line_count):
-            width, height = self.draw.textsize(lines[i], self.font)
-            x = self.image.width / 2 - width / 2
-            y = last_y + height
-            self.draw_text_with_outline(lines[i], x, y)
-            last_y = y
+        self.draw.text((coord_x, coord_y), text, (255, 255, 255), font=self.font)
 
 
 if __name__ == '__main__':
     meme_generator = MemeGenerator()
-    meme_generator.add_image('image.jpeg')
-    meme_generator.add_caption("One does not simplyfd fdf dfgtr rt hrhrh rth rth rt")
-    meme_generator.add_caption("One does not simplyfd fdf dfgtr rt hrhrh rth rth rt ")
-
-    meme_generator.generate()
+    meme_generator.add_something(1, image='image.jpeg')
+    meme_generator.add_something(2, caption="One does not simplyfd")
+    generator = meme_generator.add_something(3, caption="One does not")
+    if generator.ready():
+        print(meme_generator.generate())
